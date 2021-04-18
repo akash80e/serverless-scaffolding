97917197adf378af4323b5b54c7a197c1419dc55
Akash Bharti <akash80e@gmail.com>
2021-04-17 21:35:37 -0400

    add prettier and eslint

--- src/ui/lambdas/Consumer.ts
+++ src/ui/lambdas/Consumer.ts
@@ -1,11 +1,5 @@
-import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult, SQSEvent, SQSHandler} from 'aws-lambda';
+import { SQSEvent, SQSHandler } from "aws-lambda";
 
 export const handler: SQSHandler = async (event: SQSEvent): Promise<void> => {
-
-    try {
-        console.log(event);
-    } catch (error) {
-        throw error;
-    }
-
-}
+  console.log(event.Records);
+};
